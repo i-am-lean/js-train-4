8d0c7c1a791ba18c429c9deb913f3037543e3d44
i-am-lean <alina.gorbenko.97@gmail.com>
2023-12-14 16:51:27 +0200

    end 4 task

---
+++ index copy.js
@@ -0,0 +1,13 @@
+
+
+// Завдання 17: Напишіть функцію, яка видаляє частину рядка між двома індексами використовуючи substring
+
+function substringString(string, startIndex, endIndex) {
+  // Використовуємо метод substring(), щоб видалити частину рядка string між двома індексами startIndex та endIndex
+}
+
+console.log("Завдання 17 ====================================");
+console.log(
+  'substringString("Hello, world!", 0, 5)',
+  substringString("Hello, world!", 0, 5)
+); // Виведе 'Hello'
